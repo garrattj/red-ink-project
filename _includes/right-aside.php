ffd43d9b7157d92458b0ff9e1bff1f5c51f3f687
Jonathan Garratt <Jonathan Garratt>
2016-05-07 00:02:37 +1000

    started adding right aside content

--- _includes/right-aside.php
+++ _includes/right-aside.php
@@ -1,32 +1,20 @@
 <div class="col-md-4 max-margin-top">
 	<div class="panel panel-default">
 		<div id="panelhead">
-			<div class="panel-heading">PAST&UPCOMINGEVENTSA</div>
+			<div class="panel-heading">PAST&UPCOMINGEVENTS</div>
 		</div>
-			<div class="panel-body">
-				<p>6th February 2015 (Friday) <br> New Found Glory live in Kuala Lumpur! <br> Bentley Music Auditorium</p>
-				<p>5th February 2015 (Thursday)<br> Hang out with New Found Glory!<br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
-				<p>17th August 2014 (Sunday) <br> Majlis Jamuan Hari Raya 2014 <br> Putra Walk, Equine Seri Kembangan</p>
-				<p>2nd May 2014 (Friday) <br> Is This Ska? Kuala Lumpur 2014 <br> Insider Space, Kuala Lumpur </p>
-				<p>26th January 2013 (Saturday) <br> Comeback Kid : Live in Kuala Lumpur <br> OVO Live Bistro, Damansara Selangor</p>
-
-
-			</div>
-	</div>
-
-	<div class="panel panel-default">
-		<div id="panelhead">
-			<div class="panel-heading">Facebook</div>
-		</div>
-			<div class="panel-body">
-				<div class="fb-page" data-href="https://www.facebook.com/pakenhamsilverbacks" data-tabs="timeline" data-width="300" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/pakenhamsilverbacks"><a href="https://www.facebook.com/pakenhamsilverbacks"></a></blockquote></div></div>
-			</div>
-	</div>
-
-	<div class="panel panel-default">
 		<div class="panel-body">
-			<img src="assets/images/dest/silverbacks-logo-big.jpg" width="250px" height="250px">
-			<figcaption class="wp-caption-text">Gridiron Pakenham Silverbacks</figcaption>
+			<p>6th February 2015 (Friday) <br> New Found Glory live in Kuala Lumpur! <br> Bentley Music Auditorium</p>
+			<p>5th February 2015 (Thursday)<br> Hang out with New Found Glory!<br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
+			<p>17th August 2014 (Sunday) <br> Majlis Jamuan Hari Raya 2014 <br> Putra Walk, Equine Seri Kembangan</p>
+			<p>2nd May 2014 (Friday) <br> Is This Ska? Kuala Lumpur 2014 <br> Insider Space, Kuala Lumpur </p>
+			<p>26th January 2013 (Saturday) <br> Comeback Kid : Live in Kuala Lumpur <br> OVO Live Bistro, Damansara Selangor</p>
 		</div>
+		<div class="panel-footer">VIEW ALL EVENTS</div>
 	</div>
 </div>
+
+	
+
+	
+
