f9e7e12af21763b8a448948330575b2f3ca3a619
Jonathan Garratt <Jonathan Garratt>
2016-05-06 23:56:08 +1000

    created basic header, working links and started index main content. Created an includes file for header, footer and right-aside

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<title>REDINK ENTERTAINMENT</title>
+    <!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">REDINK ENTERTAINMENT</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">HOME<span class="sr-only">(current)</span></a></li>
+        <li><a href="bands.php">BANDS</a></li>
+        <li><a href="videos.php">VIDEOS</a></li>
+		<li><a href="events.php">EVENTS</a></li>
+		<li><a href="services.php">SERVICES</a></li>
+		<li><a href="#contact.php">CONTACT</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div id="bigwrapper" class="container no-padding-all">
+			<div class="ticker">
+				<span class="ticker-heading">Latest</span>
+			</div>
+
+			<div class="container">
+				<div class="content">
+					<div class="row">
+						<div class="col-md-8">
+							<div class= "main-content">
+								<h2>LATEST NEWS</h2>
+								<h5>WE WANT YOUR BAND. JOIN US NOW!</h5>
+								<p> 31/01/2016 </p>
+								<p>Siapa masih ingat pada tahun 2007 – 2009 dulu?</p>
+								<p>Scene kita amat meriah! Band bertimbun, show merata, dari pop hingga ke metal dan semua orang termasuk saluran media aliran perdana bercakap tentang indie scene.</p>
+								<p>Selepas tahun 2009, scene kita kembali suam-suam kuku hingga ke hari ini. Band yang dulunya tumbuh macam cendawan hilang entah ke mana. Event organizer, clothing label, jurugambar dan komuniti bebas juga melenyap.</p>
+								<p>Dulunya satu show boleh mencecah lebih 1000 orang di MCPA, hari ini 60 orang pun dah kira ramai. Jangan cerita pasal untung, kebanyakkan event organizer hari ini sanggup belanja duit gaji untuk rugi demi kelangsungan independent movement yang dicintai.</p>
+								<p>Setelah hampir 10 tahun semenjak tahun 2007, banyak label kawan-kawan tak putus asa menghidupkan scene masing-masing. Itu yang kita perlu respek!</p>
+								<p>Tahu tak korang? Baru-baru ini Two Decades of Rebellion : Subculture X ACAB Tour 2016 dapat sambutan hangat. Show pertama mereka di Hard Rock Cafe fullhouse bro!</p>
+								<p>Tambahan pula, ROTTW Soundstage final episode juga bakal menggegar lagi. Kitaorang fikir adakah ini satu petanda baru?</p>
+								<p>Adakah sumpahan 10 tahun kepada indie scene bakal berakhir? Mahukah kita semua bersama-sama mencipta semula gelombang indie ke seluruh tanah air seperti zaman lalu?</p>
+								<p>Andai ya! Kitaorang rasa inilah masanya. Kitaorang pun tengah semangat giler babs.</p>
+								<p>Banyak projek rilis CD bakal menyusul, gig lokal dan band oversea pun sedang disusun oleh tim kitaorang.</p>
+								<p>Jadi, mana korang yang ada band, boleh lah kembali aktif sebab kitaorang tengah cari band untuk pelbagai show dalam masa terdekat ini.</p>
+								<p>Serius, nak lebih mudah korang boleh register band korang kat link bawah ini —-> http://goo.gl/forms/H3Xjjrw6f9</p>
+								<p>Tak cuba tak tahu kan? Thanks, jumpa korang lagi beb!</p>
+								<p></p>
+						
+							</div> <!-- END main-content -->
+						</div> <!-- END col-md-8 -->
+		<?php include ("_includes/right-aside.php") ?>
+					</div> <!-- END row -->
+				</div> <!-- END content -->
+			</div>  <!-- END container -->
+		</div>
