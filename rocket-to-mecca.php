ac25dcca40f87ef4266365ae1e143b685a3206a0
Jonathan Garratt <Jonathan Garratt>
2016-05-23 22:51:17 +1000

    added content rocket to mecca page

--- rocket-to-mecca.php
+++ rocket-to-mecca.php
@@ -2,8 +2,28 @@
   include ("_includes/header.php")
 ?>
 <div class="container">
-	<div class="content">
-		<h2> ROCKET TO MECCA</h2>
-		<h5>POP/ROCK/ALTERNATIVE</h5>
+	<div class="content"><div class="row">
+			<div class="col-md-6">
+				<h2>ROCKET TO MECCA</h2>
+				<h4>POP/ROCK/ALTERNATIVE</h4>
+				<img src='assets/images/slider-4.jpg' alt='missing' height="320" width="481" />
+				<h4>BAND MEMBERS</h4>
+				<p>Mohd Faris (Vocal)</p>
+				<p>Mohd Anwar (Bass)</p>
+				<p>Mahzan Janudin (Lead Guitar)</p>
+				<p>Fauzie Abdullah (Guitar)</p>
+				<p>Fouzy Ismail (Drum)</p>
+				
+				<h4>EXTERNAL LINKS</h4>
+				<a href="http://www.facebook.com/rockettomecca">http://www.facebook.com/rockettomecca</a>
+			</div>
+			<div class="col-md-6">
+				<h4>BIOGRAPHY</h4>
+					<p>Rocket To Mecca is a rock / alternative band from Johor Bahru since 2012. More info about us will be updated real soon. :)</P>
+				<div class="button">
+					<a href="bands.php"><button type="button">BROWSE ALL BANDS</button></a>
+				</div>	
+			</div>
+		</div>
 	</div>
 </div>
