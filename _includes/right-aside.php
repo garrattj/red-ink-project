67786e009e8a9bc57e64ff5ee600560c1728c426
Jonathan Garratt <Jonathan Garratt>
2016-06-20 22:04:59 +1000

    style front page worked on right-aside and got that all set up

--- _includes/right-aside.php
+++ _includes/right-aside.php
@@ -1,56 +1,76 @@
 
-	<div class="panel panel-default">
+<div class="panel panel-default">
+	<div class="search">
 		<div class="panel-body">
-			<div id="search">
-				<form action="action_page.php">
-					<input type="search" name="googlesearch">
-					<input type="submit">
-				</form>
-			</div>
+			
+			<form action="action_page.php">
+				<input type="search" name="googlesearch">
+					
+			</form>
 		</div>
 	</div>
-	<div class="panel panel-default">
-		<div id="panelhead">
-			<div class="panel-heading">
-				<h3>PAST&UPCOMING <span>EVENTS</span> </h3>
-			</div>
-		</div>
-		<div class="panel-body">
-			<p>6th February 2015 (Friday) <br> New Found Glory live in Kuala Lumpur! <br> Bentley Music Auditorium</p>
-			<p>5th February 2015 (Thursday)<br> Hang out with New Found Glory!<br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
-			<p>17th August 2014 (Sunday) <br> Majlis Jamuan Hari Raya 2014 <br> Putra Walk, Equine Seri Kembangan</p>
-			<p>2nd May 2014 (Friday) <br> Is This Ska? Kuala Lumpur 2014 <br> Insider Space, Kuala Lumpur </p>
-			<p>26th January 2013 (Saturday) <br> Comeback Kid : Live in Kuala Lumpur <br> OVO Live Bistro, Damansara Selangor</p>
-		</div>
-		<div class="panel-footer">VIEW ALL EVENTS</div>
-	</div>
-	<div class="panel panel-default">
-		<div id="panelhead">
-			<div class="panel-heading"> FEATURED VIDEOS </div>
-		</div>
-		<div class="panel-body">
-			<iframe width="310" height="207" src="https://www.youtube.com/embed/jgUn_xXRtF0" frameborder="0" allowfullscreen></iframe>
-			<iframe width="310" height="207" src="https://www.youtube.com/embed/Qu1Hw8WJeHo" frameborder="0" allowfullscreen></iframe>		
-		</div>
-		<div class="panel-footer">
-			<a href="videos.php"> VIEW ALL VIDEOS</a></div>
-	</div>
-	<div class="panel panel-default">
-		<div id="panelhead">
-			<div class="panel-heading"> FEATURED BANDS </div>
-		</div>
-		<div class="panel-body">
-			<p>6th February 2015 (Friday) <br> New Found Glory live in Kuala Lumpur! <br> Bentley Music Auditorium</p>
-			<p>5th February 2015 (Thursday)<br> Hang out with New Found Glory!<br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
-			<p>17th August 2014 (Sunday) <br> Majlis Jamuan Hari Raya 2014 <br> Putra Walk, Equine Seri Kembangan</p>
-			<p>2nd May 2014 (Friday) <br> Is This Ska? Kuala Lumpur 2014 <br> Insider Space, Kuala Lumpur </p>
-			<p>26th January 2013 (Saturday) <br> Comeback Kid : Live in Kuala Lumpur <br> OVO Live Bistro, Damansara Selangor</p>
-		</div>
-		<div class="panel-footer">
-			<a href="bands.php">VIEW ALL BANDS</a>
-		</div>
-	</div>
-
-
+</div>
+	
+	
 	
 
+  
+  
+<div class="panel panel-default panel-redink">
+	<div class="panel-heading">
+		<h3 class="panel-title"><h3>PAST&UPCOMING <span>EVENTS</span> </h3></h3>
+	</div>
+	<div class="panel-body">
+		<p>6th February 2015 (Friday) <br><span> New Found Glory live in Kuala Lumpur!</span> <br> Bentley Music Auditorium</p>
+		<p>5th February 2015 (Thursday)<br><span> Hang out with New Found Glory!</span><br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
+		<p>17th August 2014 (Sunday) <br><span> Majlis Jamuan Hari Raya 2014 </span><br> Putra Walk, Equine Seri Kembangan</p>
+		<p>2nd May 2014 (Friday) <br> <span>Is This Ska? Kuala Lumpur 2014 </span><br> Insider Space, Kuala Lumpur </p>
+		<p>26th January 2013 (Saturday) <br><span> Comeback Kid : Live in Kuala Lumpur </span><br> OVO Live Bistro, Damansara Selangor</p>
+	</div>
+</div>
+
+
+
+<div class="panel panel-default panel-redink">
+	<div class="panel-heading">
+		<h3 class="panel-title"><h3>FEATURED <span>VIDEOS</span> </h3></h3>
+	</div>
+	<div class="panel-body">
+		<p>6th February 2015 (Friday) <br><span> New Found Glory live in Kuala Lumpur!</span> <br> Bentley Music Auditorium</p>
+		<p>5th February 2015 (Thursday)<br><span> Hang out with New Found Glory!</span><br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
+		<p>17th August 2014 (Sunday) <br><span> Majlis Jamuan Hari Raya 2014 </span><br> Putra Walk, Equine Seri Kembangan</p>
+		<p>2nd May 2014 (Friday) <br> <span>Is This Ska? Kuala Lumpur 2014 </span><br> Insider Space, Kuala Lumpur </p>
+		<p>26th January 2013 (Saturday) <br><span> Comeback Kid : Live in Kuala Lumpur </span><br> OVO Live Bistro, Damansara Selangor</p>
+	</div>
+</div>
+
+
+
+<div class="panel panel-default panel-redink">
+	<div class="panel-heading">
+		<h3 class="panel-title"><h3>FEATURED <span>BANDS</span> </h3></h3>
+	</div>
+	<div class="panel-body">
+		<p>6th February 2015 (Friday) <br><span> New Found Glory live in Kuala Lumpur!</span> <br> Bentley Music Auditorium</p>
+		<p>5th February 2015 (Thursday)<br><span> Hang out with New Found Glory!</span><br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
+		<p>17th August 2014 (Sunday) <br><span> Majlis Jamuan Hari Raya 2014 </span><br> Putra Walk, Equine Seri Kembangan</p>
+		<p>2nd May 2014 (Friday) <br> <span>Is This Ska? Kuala Lumpur 2014 </span><br> Insider Space, Kuala Lumpur </p>
+		<p>26th January 2013 (Saturday) <br><span> Comeback Kid : Live in Kuala Lumpur </span><br> OVO Live Bistro, Damansara Selangor</p>
+	</div>
+</div>
+
+
+
+
+<div class="panel panel-default panel-redink">
+	<div class="panel-heading">
+		<h3 class="panel-title"><h3>PAST&UPCOMING <span>EVENTS</span> </h3></h3>
+	</div>
+	<div class="panel-body">
+		<p>6th February 2015 (Friday) <br><span> New Found Glory live in Kuala Lumpur!</span> <br> Bentley Music Auditorium</p>
+		<p>5th February 2015 (Thursday)<br><span> Hang out with New Found Glory!</span><br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
+		<p>17th August 2014 (Sunday) <br><span> Majlis Jamuan Hari Raya 2014 </span><br> Putra Walk, Equine Seri Kembangan</p>
+		<p>2nd May 2014 (Friday) <br> <span>Is This Ska? Kuala Lumpur 2014 </span><br> Insider Space, Kuala Lumpur </p>
+		<p>26th January 2013 (Saturday) <br><span> Comeback Kid : Live in Kuala Lumpur </span><br> OVO Live Bistro, Damansara Selangor</p>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -12,117 +12,119 @@
 		<link rel="stylesheet" href="assets/css/dest/custom.min.css">
 	</head>
 <body>
-<nav class="navbar navbar-default">
+	<div class="background">
+		<nav class="navbar navbar-default">
 
 
-<!-- Brand and toggle get grouped for better mobile display -->
-	<div class="navbar-header">
+		<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
 
-		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-		<a class="navbar-brand" href="#">REDINK ENTERTAINMENT</a>
-<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="index.php">HOME<span class="sr-only">(current)</span></a></li>
-				<li><a href="about.php">ABOUT</a></li>
-				<li><a href="bands.php">BANDS</a></li>
-				<li><a href="videos.php">VIDEOS</a></li>
-				<li><a href="events.php">EVENTS</a></li>
-				<li><a href="services.php">SERVICES</a></li>
-				<li><a href="contact.php">CONTACT</a></li>
-			</ul>
-		</div><!-- /.navbar-collapse -->
-	</div>
-	
-</nav>
-	<div class="container">
-		<div class="content">
-			<div class="row">
-				<div class="col-md-8">
-					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-					
-						<!-- Wrapper for slides -->
-						<div class="carousel-inner" role="listbox">
-							<div class="item active">
-								<img src="assets/images/slider-1.jpg" height="308" width="638" alt="...">
-								<div class="carousel-caption">
-									<div class ="caption">
-										Iman's League
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<img src="assets/images/slider-2.jpg" height="308" width="638" alt="...">
-								<div class="carousel-caption">
-									<div class ="caption">
-										Carbon 4 Teen
-									</div>
-								</div>
-							</div>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><span>REDINK</span> ENTERTAINMENT</a>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.php">HOME<span class="sr-only">(current)</span></a></li>
+						<li><a href="about.php">ABOUT</a></li>
+						<li><a href="bands.php">BANDS</a></li>
+						<li><a href="videos.php">VIDEOS</a></li>
+						<li><a href="events.php">EVENTS</a></li>
+						<li><a href="services.php">SERVICES</a></li>
+						<li><a href="contact.php">CONTACT</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+			
+		</nav>
+		<div class="container">
+			<div class="content">
+				<div class="row">
+					<div class="col-md-8">
+						<div id="carousel-example-generic" class="carousel slide" data-interval="false">
 						
-							<div class="item">
-								<img src="assets/images/slider-3.jpg" height="308" width="638" alt="...">
-								<div class="carousel-caption">
-									<div class ="caption">
-										Honey B
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<img src="assets/images/slider-1.jpg" height="308" width="638" alt="...">
+									<div class="carousel-caption">
+										<div class ="caption">
+											Iman's League
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="item">
-								<img src="assets/images/slider-4.jpg" height="308" width="638"alt="...">
-								<div class="carousel-caption">
-									<div class ="caption">
-										Rocket To Mecca
-									</div>	
+								<div class="item">
+									<img src="assets/images/slider-2.jpg" height="308" width="638" alt="...">
+									<div class="carousel-caption">
+										<div class ="caption">
+											Carbon 4 Teen
+										</div>
+									</div>
+								</div>
+							
+								<div class="item">
+									<img src="assets/images/slider-3.jpg" height="308" width="638" alt="...">
+									<div class="carousel-caption">
+										<div class ="caption">
+											Honey B
+										</div>
+									</div>
+								</div>
+								<div class="item">
+									<img src="assets/images/slider-4.jpg" height="308" width="638"alt="...">
+									<div class="carousel-caption">
+										<div class ="caption">
+											Rocket To Mecca
+										</div>	
+									</div>
 								</div>
 							</div>
 						</div>
+
+	<!-- Controls -->
+
+
+
+						<div class ="header">
+							<h2>LATEST<span> NEWS</span></h2>
+						</div>
+						<div class ="h5index">
+							<h5>WE WANT YOUR BAND. JOIN US NOW!</h5>
+						</div>
+								<p> 31/01/2016 </p>
+								<p>Siapa masih ingat pada tahun 2007 – 2009 dulu?</p>
+								<p>Scene kita amat meriah! Band bertimbun, show merata, dari pop hingga ke metal dan semua orang termasuk saluran media aliran perdana bercakap tentang indie scene.</p>
+								<p>Selepas tahun 2009, scene kita kembali suam-suam kuku hingga ke hari ini. Band yang dulunya tumbuh macam cendawan hilang entah ke mana. Event organizer, clothing label, jurugambar dan komuniti bebas juga melenyap.</p>
+								<p>Dulunya satu show boleh mencecah lebih 1000 orang di MCPA, hari ini 60 orang pun dah kira ramai. Jangan cerita pasal untung, kebanyakkan event organizer hari ini sanggup belanja duit gaji untuk rugi demi kelangsungan independent movement yang dicintai.</p>
+								<p>Setelah hampir 10 tahun semenjak tahun 2007, banyak label kawan-kawan tak putus asa menghidupkan scene masing-masing. Itu yang kita perlu respek!</p>
+								<p>Tahu tak korang? Baru-baru ini Two Decades of Rebellion : Subculture X ACAB Tour 2016 dapat sambutan hangat. Show pertama mereka di Hard Rock Cafe fullhouse bro!</p>
+								<p>Tambahan pula, ROTTW Soundstage final episode juga bakal menggegar lagi. Kitaorang fikir adakah ini satu petanda baru?</p>
+								<p>Adakah sumpahan 10 tahun kepada indie scene bakal berakhir? Mahukah kita semua bersama-sama mencipta semula gelombang indie ke seluruh tanah air seperti zaman lalu?</p>
+								<p>Andai ya! Kitaorang rasa inilah masanya. Kitaorang pun tengah semangat giler babs.</p>
+								<p>Banyak projek rilis CD bakal menyusul, gig lokal dan band oversea pun sedang disusun oleh tim kitaorang.</p>
+								<p>Jadi, mana korang yang ada band, boleh lah kembali aktif sebab kitaorang tengah cari band untuk pelbagai show dalam masa terdekat ini.</p>
+								<p>Serius, nak lebih mudah korang boleh register band korang kat link bawah ini —-> http://goo.gl/forms/H3Xjjrw6f9</p>
+								<p>Tak cuba tak tahu kan? Thanks, jumpa korang lagi beb!</p>
+
+								<h5>Iman's League - Anugerah Band Terbaik !</h5>
+								<ul class="pagination">
+									<li><a href="index.php">1</a></li>
+									<li><a href="index-2.php">2</a></li>
+								</ul>
+					</div>							
+					
+					<div class="col-md-4">
+						<?php
+							include ("_includes/right-aside.php")
+						?>
 					</div>
-
-<!-- Controls -->
-
-
-
-					<div class ="header">
-						<h2>LATEST<span> NEWS</span></h2>
-					</div>
-					<div class ="h5index">
-						<h5>WE WANT YOUR BAND. JOIN US NOW!</h5>
-					</div>
-							<p> 31/01/2016 </p>
-							<p>Siapa masih ingat pada tahun 2007 – 2009 dulu?</p>
-							<p>Scene kita amat meriah! Band bertimbun, show merata, dari pop hingga ke metal dan semua orang termasuk saluran media aliran perdana bercakap tentang indie scene.</p>
-							<p>Selepas tahun 2009, scene kita kembali suam-suam kuku hingga ke hari ini. Band yang dulunya tumbuh macam cendawan hilang entah ke mana. Event organizer, clothing label, jurugambar dan komuniti bebas juga melenyap.</p>
-							<p>Dulunya satu show boleh mencecah lebih 1000 orang di MCPA, hari ini 60 orang pun dah kira ramai. Jangan cerita pasal untung, kebanyakkan event organizer hari ini sanggup belanja duit gaji untuk rugi demi kelangsungan independent movement yang dicintai.</p>
-							<p>Setelah hampir 10 tahun semenjak tahun 2007, banyak label kawan-kawan tak putus asa menghidupkan scene masing-masing. Itu yang kita perlu respek!</p>
-							<p>Tahu tak korang? Baru-baru ini Two Decades of Rebellion : Subculture X ACAB Tour 2016 dapat sambutan hangat. Show pertama mereka di Hard Rock Cafe fullhouse bro!</p>
-							<p>Tambahan pula, ROTTW Soundstage final episode juga bakal menggegar lagi. Kitaorang fikir adakah ini satu petanda baru?</p>
-							<p>Adakah sumpahan 10 tahun kepada indie scene bakal berakhir? Mahukah kita semua bersama-sama mencipta semula gelombang indie ke seluruh tanah air seperti zaman lalu?</p>
-							<p>Andai ya! Kitaorang rasa inilah masanya. Kitaorang pun tengah semangat giler babs.</p>
-							<p>Banyak projek rilis CD bakal menyusul, gig lokal dan band oversea pun sedang disusun oleh tim kitaorang.</p>
-							<p>Jadi, mana korang yang ada band, boleh lah kembali aktif sebab kitaorang tengah cari band untuk pelbagai show dalam masa terdekat ini.</p>
-							<p>Serius, nak lebih mudah korang boleh register band korang kat link bawah ini —-> http://goo.gl/forms/H3Xjjrw6f9</p>
-							<p>Tak cuba tak tahu kan? Thanks, jumpa korang lagi beb!</p>
-
-							<h5>Iman's League - Anugerah Band Terbaik !</h5>
-							<ul class="pagination">
-								<li><a href="index.php">1</a></li>
-								<li><a href="index-2.php">2</a></li>
-							</ul>
-				</div>							
-				
-				<div class="col-md-4">
-					<?php
-						include ("_includes/right-aside.php")
-					?>
 				</div>
-			</div>
-		</div>	
+			</div>	
+		</div>
 	</div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
