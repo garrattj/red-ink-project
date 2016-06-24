dc86445c98aca75ae6bc55e3e156e5e0f48b5ff4
Adam Kolinski <akolinski@nurdit.com>
2016-06-25 00:58:09 +1000

    navigation bug fix and panel border

--- index.php
+++ index.php
@@ -4,46 +4,52 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<title>Red Ink Entertainment</title>
-<!-- Bootstrap -->
+		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<!-- Font awesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+		<!-- Custom fonts and custom style sheet -->
 		<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
-		<script src="https://use.fontawesome.com/df7e2c07e1.js"></script>
 		<link rel="stylesheet" href="assets/css/dest/custom.min.css">
+
 	</head>
 <body>
-	<nav id="navbar-redink" class="navbar navbar-default">
-	  <div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		  </button>
-		  <a class="navbar-brand" href="#"><span class="main-title">Redink </span>Entertainment</a>
-		</div>
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		  <ul class="nav navbar-nav">
-			<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-			<li><a href="#">About</a></li>
-			<li><a href="#">Bands</a></li>
-			<li><a href="#">Videos</a></li>
-			<li><a href="#">Events</a></li>
-			<li><a href="#">Services</a></li>
-			<li><a href="#">Contact</a></li>
-		  </ul>
-		</div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
+	<div id="redink-navbar">
+		<nav class="navbar navbar-default">
+		  <div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="#"><span class="main-title">Redink </span>Entertainment</a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
+			  <ul class="nav navbar-nav">
+					<li class="active"><a href="/" style="color: #FFF;">Home <span class="sr-only">(current)</span></a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Bands</a></li>
+					<li><a href="#">Videos</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Contact</a></li>
+			  </ul>
+			</div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+
 		<div class="container content-area-background">
 				<div class="row">
 					<div class="col-md-8">
 						<div id="carousel-example-generic" class="carousel slide" data-interval="false">
-						
+
 							<!-- Wrapper for slides -->
 							<div class="carousel-inner" role="listbox">
 								<div class="item active">
@@ -62,7 +68,7 @@
 										</div>
 									</div>
 								</div>
-							
+
 								<div class="item">
 									<img src="assets/images/slider-3.jpg" class="img img-responsive" alt="...">
 									<div class="carousel-caption carousel-redink-caption">
@@ -76,17 +82,17 @@
 									<div class="carousel-caption carousel-redink-caption">
 										<div class="caption">
 											Rocket To Mecca
-										</div>	
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 
 	<!-- Controls -->
-	
-						<h2>LATEST<span class="text-red"> NEWS</span></h2>						
+
+						<h2>LATEST<span class="text-red"> NEWS</span></h2>
 						<div class="latest-news-article">
-							<h5> <span class="text-red">WE WANT YOUR BAND. JOIN US NOW!</span></h5>						
+							<h5> <span class="text-red">WE WANT YOUR BAND. JOIN US NOW!</span></h5>
 							<p> 31/01/2016 </p>
 							<p>Siapa masih ingat pada tahun 2007 – 2009 dulu?</p>
 							<p>Scene kita amat meriah! Band bertimbun, show merata, dari pop hingga ke metal dan semua orang termasuk saluran media aliran perdana bercakap tentang indie scene.</p>
@@ -106,11 +112,11 @@
 								<li><a href="index.php">1</a></li>
 								<li><a href="index-2.php">2</a></li>
 							</ul>
-						</div>	
-								
-								
-					</div>							
-					
+						</div>
+
+
+					</div>
+
 					<div class="col-md-4">
 						<?php
 							include ("_includes/right-aside.php")
@@ -138,7 +144,7 @@
 								  <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
 								</span>
 							</div>
-							<div class="row footnote">	
+							<div class="row footnote">
 								<i class="fa fa-copyright" aria-hidden="true"></i> copyright 2016 <a class="text-red" href="index.php">Red Ink Entertainment.</a> All Rights Reserved.
 							</div>
 						</div>
