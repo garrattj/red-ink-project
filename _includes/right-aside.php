280a25d35c998f3494533a08e180e7dfdff6a1c3
Jonathan Garratt <Jonathan Garratt>
2016-06-25 12:45:12 +1000

    added panel footers, styled panel

--- _includes/right-aside.php
+++ _includes/right-aside.php
@@ -11,7 +11,7 @@
 	
 <div class="panel panel-default panel-redink">
 	<div class="panel-heading">
-		<h3 class="panel-title"><h3>PAST&UPCOMING <span>EVENTS</span> </h3></h3>
+		<h3 class="panel-title"><h3>PAST&UPCOMING <span class="text-red">EVENTS</span> </h3></h3>
 	</div>
 	<div class="panel-body">
 		<p>6th February 2015 (Friday) <br><span class="text-red"> New Found Glory live in Kuala Lumpur!</span> <br> Bentley Music Auditorium</p>
@@ -20,25 +20,27 @@
 		<p>2nd May 2014 (Friday) <br> <span class="text-red">Is This Ska? Kuala Lumpur 2014 </span><br> Insider Space, Kuala Lumpur </p>
 		<p>26th January 2013 (Saturday) <br><span class="text-red"> Comeback Kid : Live in Kuala Lumpur </span><br> OVO Live Bistro, Damansara Selangor</p>
 	</div>
+	<div class="panel-footer">VIEW ALL EVENTS</div>
 </div>
 
 
 
 <div class="panel panel-default panel-redink">
 	<div class="panel-heading">
-		<h3 class="panel-title"><h3>FEATURED <span>VIDEOS</span> </h3></h3>
+		<h3 class="panel-title"><h3>FEATURED <span class="text-red">VIDEOS</span> </h3></h3>
 	</div>
 	<div class="panel-body">
 		<iframe class="img img-responsive" src="https://www.youtube.com/embed/jgUn_xXRtF0" frameborder="0" allowfullscreen></iframe>
 		<iframe class="img img-responsive" src="https://www.youtube.com/embed/Qu1Hw8WJeHo" frameborder="0" allowfullscreen></iframe>
 	</div>
+	<div class="panel-footer">VIEW ALL VIDEOS</div>
 </div>
 
 
 
 <div class="panel panel-default panel-redink">
 	<div class="panel-heading">
-		<h3 class="panel-title"><h3>FEATURED <span>BANDS</span> </h3></h3>
+		<h3 class="panel-title"><h3>FEATURED <span class="text-red">BANDS</span> </h3></h3>
 	</div>
 	<div class="panel-body">
 		<p>6th February 2015 (Friday) <br><span> New Found Glory live in Kuala Lumpur!</span> <br> Bentley Music Auditorium</p>
@@ -47,20 +49,5 @@
 			<p>2nd May 2014 (Friday) <br><span> Is This Ska? Kuala Lumpur 2014</span> <br> Insider Space, Kuala Lumpur </p>
 			<p>26th January 2013 (Saturday) <br> <span>Comeback Kid : Live in Kuala Lumpur</span> <br> OVO Live Bistro, Damansara Selangor</p>
 	</div>
-</div>
-
-
-
-
-<div class="panel panel-default panel-redink">
-	<div class="panel-heading">
-		<h3 class="panel-title"><h3>PAST&UPCOMING <span>EVENTS</span> </h3></h3>
-	</div>
-	<div class="panel-body">
-		<p>6th February 2015 (Friday) <br><span> New Found Glory live in Kuala Lumpur!</span> <br> Bentley Music Auditorium</p>
-		<p>5th February 2015 (Thursday)<br><span> Hang out with New Found Glory!</span><br> Alt HQ, Bukit Bintang, Kuala Lumpur.</p>
-		<p>17th August 2014 (Sunday) <br><span> Majlis Jamuan Hari Raya 2014 </span><br> Putra Walk, Equine Seri Kembangan</p>
-		<p>2nd May 2014 (Friday) <br> <span>Is This Ska? Kuala Lumpur 2014 </span><br> Insider Space, Kuala Lumpur </p>
-		<p>26th January 2013 (Saturday) <br><span> Comeback Kid : Live in Kuala Lumpur </span><br> OVO Live Bistro, Damansara Selangor</p>
-	</div>
+	<div class="panel-footer">VIEW ALL BANDS</div>
 </div>
