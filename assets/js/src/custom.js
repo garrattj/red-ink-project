20edcbaac46ae6d9caa027eb56174619e836765b
Jonathan Garratt <Jonathan Garratt>
2016-07-01 22:52:20 +1000

    added tabs underlined to custom js file

--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -1,1 +1,27 @@
 // Custom javascript for Redink
+
+// Tabs underlined js
+$(document).ready(function() {
+
+	var widget = $('.tabs-underline');
+
+	var tabs = widget.find('ul a'),
+		content = widget.find('.tabs-content-placeholder > div');
+
+	tabs.on('click', function (e) {
+
+		e.preventDefault();
+
+		// Get the data-index attribute, and show the matching content div
+
+		var index = $(this).data('index');
+
+		tabs.removeClass('tab-active');
+		content.removeClass('tab-content-active');
+
+		$(this).addClass('tab-active');
+		content.eq(index).addClass('tab-content-active');
+
+	});
+
+});
